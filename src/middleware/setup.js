dd9aba25583d44082349b5aa4ce948c9a39a4fd4
Bubunyo Nyavor <bubu@afradio.co>
2019-12-06 11:10:51 +0000

    Initial commit, create booilerplate

---
+++ src/middleware/setup.js
@@ -0,0 +1,20 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import errorhandler from 'errorhandler';
+import morgan from 'morgan';
+import methodOverride from 'method-override';
+import { isDev } from '../config/constants';
+
+
+export default (app) => {
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+
+  if (isDev()) {
+    app.use(morgan('dev'));
+    app.use(errorhandler());
+  }
+};
---
+++ src/modules/api/api.routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { version } from '../../../package.json';
+import UserRouter from '../user/user.routes';
+
+// Declare Router
+const apiRouter = Router();
+
+// get version number of  the api
+apiRouter.get('/', (req, res) => {
+  res.json({ version });
+});
+
+// Plug module routers
+
+//
+
+export default apiRouter;
